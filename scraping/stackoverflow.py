40547b09b9462499d5475db413d83d431eebebbe
André Felype <109329430+felypecoliveira@users.noreply.github.com>
2023-05-20 05:48:33 -0300

    scraping script

--- scraping/stackoverflow.py
+++ scraping/stackoverflow.py
@@ -8,6 +8,6 @@ response = requests.get(url)
 html = BeautifulSoup(response.text, 'html.parser')
 
 for pergunta in html.select('.s-post-summary--content'):
-    data = pergunta.select_one('.relativetime')
+    datas = pergunta.select_one('.relativetime')
     head = pergunta.select_one('.s-link')
-    print(data.text, head.text, sep='\t')
+    print(datas.text, head.text, sep='\t')
