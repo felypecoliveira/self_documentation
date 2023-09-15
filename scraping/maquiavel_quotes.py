aeaa76b84cb1a7aac3b5e55d4855df3535b121a2
Usuário <andrefelype2015@gmail.com>
2023-09-15 00:51:29 -0300

    replace the last one

--- scraping/maquiavel_quotes.py
+++ scraping/maquiavel_quotes.py
@@ -5,10 +5,21 @@ import requests
 url = "https://www.pensador.com/frases_de_maquiavel/"
 
 
+
 response  = requests.get(url)
+
+
 soup = BeautifulSoup(response.text, 'html.parser')
+
+
+
 conteiner = soup.find(class_='phrases-list')
+
+
+
 items = conteiner.find_all('p')
+
+
 
 for item in items:
     print(item.text)
