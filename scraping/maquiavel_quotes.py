e1dd659ba6bedcf791735caf19941710ec346af9
Usuário <andrefelype2015@gmail.com>
2023-09-15 01:04:32 -0300

    replace the last one

--- scraping/maquiavel_quotes.py
+++ scraping/maquiavel_quotes.py
@@ -4,19 +4,9 @@ import requests
 
 url = "https://www.pensador.com/frases_de_maquiavel/"
 
-
-
 response  = requests.get(url)
-
-
 soup = BeautifulSoup(response.text, 'html.parser')
-
-
-
 conteiner = soup.find(class_='phrases-list')
-
-
-
 items = conteiner.find_all('p')
 
 
