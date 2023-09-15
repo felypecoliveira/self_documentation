1006d65b9cb54e025bb2110ea80ee3921a16247f
André Felype <109329430+felypecoliveira@users.noreply.github.com>
2023-09-15 01:03:00 -0300

    Update maquiavel_quotes.py

--- scraping/quotes.py
+++ scraping/quotes.py
@@ -1,5 +1,14 @@
-from bs4 import BeautifulSoup as bs
-import re
+from bs4 import BeautifulSoup
 import requests
 
 
+url = "https://www.pensador.com/frases_de_maquiavel/"
+
+
+response  = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+conteiner = soup.find(class_='phrases-list')
+items = conteiner.find_all('p')
+
+for item in items:
+    print(item.text)
