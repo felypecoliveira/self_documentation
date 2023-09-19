0714de9efe06fd74f378b43c9e20cb0a99fcf5a9
Usuário <andrefelype2015@gmail.com>
2023-09-18 23:45:01 -0300

    refactor and adding some coding

--- scraping/stackoverflow.py
+++ scraping/stackoverflow.py
@@ -7,6 +7,8 @@ response = requests.get(url)
 
 html = BeautifulSoup(response.text, 'html.parser')
 
+
+
 for pergunta in html.select('.s-post-summary--content'):
     data = pergunta.select_one('.relativetime')
     head = pergunta.select_one('.s-link')
